e0ed4cade9a7ebdf2bdfddfc5713ab29ca04c6d0
Om12ek99 <omar.elkamili@gmail.com>
2024-06-04 00:30:51 +0200

    add: added imported classes to index

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// importo classi
+require_once __DIR__ . "/models/products.php";
+require_once __DIR__ . "/models/toys.php";
+require_once __DIR__ . "/models/foods.php";
+require_once __DIR__ . "/models/beds.php";
+;
+?>
