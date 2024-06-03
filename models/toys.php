cc6014e3673f4412f5f1fb65304f9462ab993564
Om12ek99 <omar.elkamili@gmail.com>
2024-06-04 00:06:28 +0200

    add:bug fxes

--- models/toys.php
+++ models/toys.php
@@ -4,14 +4,18 @@ require_once __DIR__ . "/products.php";
 
 //definizione della classe giochi
 class Toys extends Products{
-    private string $animalage;  //kitten, adult or senior
+    private string $animalAge;  //kitten, adult or senior
     private string $size;       //small or large
 
     //definizione del construct figlio
-    public function __construct(string $name, string $price, string $image, string $type,  string $animalage, string $size){
+    public function __construct(string $name, string $price, string $image, string $type,  string $animalAge, string $size){
         parent::__construct($name, $price, $type, $image);
-        $this->animalage = $animalage;
+        $this->animalAge = $animalAge;
         $this->size=$size;
     }
+        // conversione in valori visibili ed accessibili
+        public function getAnimalAge(){
+            return $this->animalAge;
+        }
 }
 ?>
