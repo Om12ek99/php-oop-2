c83ff94fce7d540ee4ade64aa3ad35b993128671
Om12ek99 <omar.elkamili@gmail.com>
2024-06-04 00:21:37 +0200

    add:class Food

---
+++ models/food.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once __DIR__ . "/products.php";
+
+class Food extends Products {
+    private bool $canned;
+    private string $name;
+    private string $mainIngredient;
+
+}
+
+?>
