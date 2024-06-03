83573303140e6ee71c97197b3718eeab2d60fa26
Om12ek99 <omar.elkamili@gmail.com>
2024-06-04 00:02:36 +0200

    add: construct toys

--- models/toys.php
+++ models/toys.php
@@ -2,8 +2,14 @@
 
 require_once __DIR__ . "/products.php";
 
-
+//definizione della classe giochi
 class Toys extends Products{
-    
+    private string $animalage;  //kitten, adult or senior
+    private string $size;       //small or large
+
+    //definizione del construct figlio
+    public function __construct(string $name, string $price, string $image, string $type,  string $animalage, string $size){
+        parent::__construct($name, $price, $type, $image);
+    }
 }
 ?>
