d4b57e4b56ddc898defbaaea23292e5f0657cc48
Om12ek99 <omar.elkamili@gmail.com>
2024-06-04 00:26:05 +0200

    add: inserimento del booleano in food

--- models/food.php
+++ models/food.php
@@ -4,9 +4,23 @@ require_once __DIR__ . "/products.php";
 
 class Food extends Products {
     private bool $canned;
-    private string $name;
+    private string $foodName;
     private string $mainIngredient;
 
+    public function __construct(string $name, string $price, string $type, string $image, string $canned, string $foodName, string $mainIngredient){
+        parent::__construct($name, $price, $type, $image); 
+
+        $this->foodName = $foodName;
+        $this->mainIngredient = $mainIngredient;
+     }
+
+     //inserimento del booleano
+     public function setCanned($canned)
+     {
+         $this->canned = $canned;
+     }
+
+     
 }
 
 ?>
